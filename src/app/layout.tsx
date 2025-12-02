cadb5d57ab5bb4220fc4e7e5b7ded072ee977610
5d-jh <jeonghyun0126@outlook.com>
2025-12-02 17:37:58 +0900

    fix miscs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
     title: "University Simulation",
     description: "University course registration simulation",
+    icons: {
+        icon: '/favicon.png',
+    },
 };
 
 export default function RootLayout({
